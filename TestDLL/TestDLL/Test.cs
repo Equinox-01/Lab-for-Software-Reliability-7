5c7cb90d42611f14fa44809324c63c4a1ea181d6
Equinox <sovereign.stas@yandex.ru>
2017-11-30 06:27:13 +0300

    Added tested class

---
+++ TestDLL/TestDLL/Test.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDLL
+{
+    public class Test<T>
+    {
+        private T data;
+
+        public void Set(T indata)
+        {
+            if (indata == null)
+                throw new ArgumentNullException("Indata is null");
+            data = indata;
+        }
+
+        public bool GetIntegerStatus()
+        {
+            if (data.ToString() == null)
+                throw new ArgumentNullException("Data is null");
+            else
+            {
+                int tmp = 0;
+                return int.TryParse(data.ToString(), out tmp);
+            }
+        }
+
+        public bool GetDoubleStatus()
+        {
+            if (data.ToString() == null)
+                throw new ArgumentNullException("Data is null");
+            else
+            {
+                double tmp = 0;
+                return double.TryParse(data.ToString(), out tmp);
+            }
+        }
+        
+        public string PrintMainMetaInfo()
+        {
+            if (data == null)
+                throw new ArgumentNullException("Data is null");
+            return "\nName " + data.GetType().FullName + "\nVisible " + data.GetType().IsVisible + "\nSerializable " + data.GetType().IsSerializable;
+        }
+
+        public void SerializeObj(string indata)
+        {
+            if (data == null)
+                throw new ArgumentNullException("Data is null");
+            if (!data.GetType().IsSerializable)
+                throw new ArgumentException("Data is not serializable");
+            var formatter = new BinaryFormatter();
+            using (var fs = new FileStream(indata, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, data);
+            }
+        }
+
+        public void DeserializeObj(string indata)
+        {
+            if (!File.Exists(indata))
+                throw new IOException("File not exist");
+            var formatter = new BinaryFormatter();
+            using (var fs = new FileStream(indata, FileMode.Open))
+            {
+                data = (T)formatter.Deserialize(fs);
+            }
+        }
+    }
+}
